541e5e15782cfd6ee557e98a8a52e24704c4ef8a
Pablo Rito <pablorito14@gmail.com>
2023-11-09 10:17:04 -0300

    correcciones

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,12 +1,12 @@
 
 import './Header.css';
-
+// 
 const Header = () => {
  return(
    <header>
     <nav className="navbar navbar-expand-lg bg-dark sticky-top" data-bs-theme="dark">
       <div className="container">
-        <a className="navbar-brand" href="/">MyApp</a>
+        <a className="navbar-brand" href="/">MyAppLogo</a>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
