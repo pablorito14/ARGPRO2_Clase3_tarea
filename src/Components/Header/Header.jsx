f7c2e7596541706042464bbadcd0c9d46390946e
Pablo Rito <pablorito14@gmail.com>
2023-11-09 17:38:50 -0300

    mejoras header

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -7,11 +7,16 @@ const Header = () => {
     <nav className="navbar navbar-expand-lg bg-dark sticky-top" data-bs-theme="dark">
       <div className="container">
         <a className="navbar-brand" href="/">MyAppLogo</a>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
+        <div className='d-flex gap-4 align-items-center order-0 order-md-1'>
+          <div className='d-flex ms-2 order-0-'>
+            <a href="/" className='shop-cart position-relative'><i className="fa-solid fa-cart-shopping fa-lg"></i></a>
+          </div>
+          <button className="navbar-toggler order-1-" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+        </div>
         
-        <div className="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
+        <div className="collapse navbar-collapse justify-content-end order-1 order-md-0 " id="navbarNavDropdown">
           <div className="navbar-nav">
             <div className="nav-item">
               <a href="/home" className="nav-link">Inicio</a>
@@ -22,16 +27,9 @@ const Header = () => {
             <div className="nav-item">
               <a href="/cont" className="nav-link">Contacto</a>
             </div>
-            {/* <div className="nav-item">
-              <div className="nav-link"><i className="fa-solid fa-cart-shopping"></i></div>
-            </div> */}
           </div>
         </div>
         
-        <div className='d-flex ms-2'>
-          <a href="/" className='shop-cart'><i className="fa-solid fa-cart-shopping fa-lg"></i></a>
-          
-        </div>
       </div>
     </nav>
    </header>
--- src/Components/Producto/Producto.jsx
+++ src/Components/Producto/Producto.jsx
@@ -37,7 +37,7 @@ const PrecioBoton = ({stock,precio}) => {
   return (
     <>
       <h3 className="fw-bold card-text">Precio: $ {precio}</h3>
-      <button className='btn px-5'>Agregar al carrito</button>
+      <button className='btn py-3 py-md-2 px-5'>Agregar al carrito</button>
     </>
   )
 }
@@ -49,19 +49,19 @@ const Producto = ({producto}) => {
   return(
     <div className="card shadow">
       <div className="row g-0">
-        <p className="w-100 text-center mb-0">
-          <small className='text-body-secondary'>SKU: {sku}</small>
-        </p>
         <div className="col-lg-4 ">
-          <div className="bg-dark- w-100 h-100 d-flex align-items-center justify-content-center justify-content-md-start">
-            <img src={`./static/media/productos/${img}`} className="img-fluid" alt={nombre} />
+          <div className="w-100 h-100 d-flex align-items-center justify-content-center">
+            <img src={`./static/media/productos/${img}`} className="img-fluid rounded" alt={nombre} />
           </div>
         </div>
         <div className="col-lg-8">
           <div className="card-body pb-0 d-flex align-items-start flex-column h-100">
             <div className='mb-auto'>
               <h2 className="card-title">{nombre}</h2>
-              <p className="card-text">{descripcion}</p>
+              <p className="card-text mb-2">{descripcion}</p>
+              <p className="w-100 mb-0">
+                <small className='text-body-secondary'>SKU: {sku}</small>
+              </p>  
             </div>
 
             <div className="my-auto py-4 w-100 d-flex flex-column align-items-center align-items-md-end">
